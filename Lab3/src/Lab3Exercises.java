d82c44bdcb3c7c896e21e637314aa79cb8013461
4010b4 <4010b4@136.145.57.226>
2017-02-08 10:01:02 -0400

    Centered the frame and made sure the program stops when closed.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,12 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("Luis Domenech");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		//Ubuntu pc resolution is 1920 x 1080
+		myFrame.setSize(200, 200);
+		//We substract the frame's dimensions to truly center the frame.
+		myFrame.setLocation(960 - myFrame.getWidth(), 540 - myFrame.getHeight());
 		myFrame.setVisible(true);
 	}
 }

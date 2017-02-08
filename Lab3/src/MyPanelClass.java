f06a7e38e22a1557024c10d744415918190fb78c
4010b4 <4010b4@136.145.57.226>
2017-02-08 11:03:16 -0400

    Added the Puerto Rican flasg :)

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -23,9 +23,9 @@ public class MyPanelClass extends JPanel {
                         int width = x2 - x1;
                         int height = y2 - y1;
  
-                        //Paint the background
-                        g.setColor(Color.BLACK);
-                        g.fillRect(x1, y1, width + 1, height + 1);
+//                        //Paint the background
+//                        g.setColor(Color.BLACK);
+//                        g.fillRect(x1, y1, width + 1, height + 1);
                         
 //                        //Draw a border
 //                        g.setColor(Color.BLUE);
@@ -65,6 +65,59 @@ public class MyPanelClass extends JPanel {
 //                        p2.addPoint(x1 + 38, y1 + 83);
 //                        g.setColor(Color.WHITE);
 //                        g.drawPolygon(p2);
+                        
+                        Polygon blue = new Polygon();
+                        blue.addPoint(0, 0);
+                        blue.addPoint(125, 75);
+                        blue.addPoint(0, 150);
+                        g.setColor(Color.BLUE);
+                        g.fillPolygon(blue);
+                        
+                        Polygon red1 = new Polygon();
+                        red1.addPoint(0, 0);
+                        red1.addPoint(210, 0);
+                        red1.addPoint(210, 33);
+                        red1.addPoint(55, 33);
+                        g.setColor(Color.RED);
+                        g.fillPolygon(red1);
+                        
+                        Polygon red2 = new Polygon();
+                        red2.addPoint(100, 60);
+                        red2.addPoint(210, 60);
+                        red2.addPoint(210, 75);
+                        red2.addPoint(125, 75);
+                        g.setColor(Color.RED);
+                        g.fillPolygon(red2);
+                        
+                        Polygon red3 = new Polygon();
+                        red3.addPoint(100, 90);
+                        red3.addPoint(210, 90);
+                        red3.addPoint(210, 75);
+                        red3.addPoint(125, 75);
+                        g.setColor(Color.RED);
+                        g.fillPolygon(red3);
+                        
+                        Polygon red4 = new Polygon();
+                        red4.addPoint(0, 150);
+                        red4.addPoint(210, 150);
+                        red4.addPoint(210, 150 - 33);
+                        red4.addPoint(55, 150 - 33);
+                        g.setColor(Color.RED);
+                        g.fillPolygon(red4);
+                        
+                        Polygon star = new Polygon();
+                        star.addPoint(x1 + 25, y1 + 73);
+                        star.addPoint(x1 + 41, y1 + 73);
+                        star.addPoint(x1 + 47, y1 + 58);
+                        star.addPoint(x1 + 53, y1 + 73);
+                        star.addPoint(x1 + 69, y1 + 73);
+                        star.addPoint(x1 + 56, y1 + 83);
+                        star.addPoint(x1 + 61, y1 + 98);
+                        star.addPoint(x1 + 47, y1 + 88);
+                        star.addPoint(x1 + 34, y1 + 98);
+                        star.addPoint(x1 + 38, y1 + 83);
+                        g.setColor(Color.WHITE);
+                        g.fillPolygon(star);
                         
                         
             }

ac6f66c170fe6202eea05d082c2040464f039f00
4010b4 <4010b4@136.145.57.226>
2017-02-08 11:02:56 -0400

    Changed frame dimensions to fit the puerto rican flag.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -5,7 +5,7 @@ public class Lab3Exercises {
 		JFrame myFrame = new JFrame("Luis Domenech");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		//Ubuntu pc resolution is 1920 x 1080
-		myFrame.setSize(200, 200);
+		myFrame.setSize(210, 150);
 		//We subtract the frame's dimensions to truly center the frame.
 		myFrame.setLocation(960 - myFrame.getWidth(), 540 - myFrame.getHeight());
 		MyPanelClass myPanel = new MyPanelClass();
